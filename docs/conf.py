f0bf2154a971d3ef285a9388f0e5f4ef16d91b67
rishabkhincha <khincharishab@gmail.com>
2019-07-24 15:42:05 -0400

    Trying to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -159,6 +159,7 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 
 # -- Adding some extensions ---------------------------------------------------
 extensions += ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
+autodoc_mock_imports = ['montage_wrapper']
 
 # -- Options for the edit_on_github extension ---------------------------------
 
